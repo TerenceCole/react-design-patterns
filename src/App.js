b499313929fd0d134c627a282056bc04bf3f90b7
Terence Cole <trcexcel@yahoo.com>
2022-07-15 12:22:59 -0500

    Updated PropertyInfo component to display more data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import Picture from "./components/Picture";
 import PropertyInfo from "./components/PropertyInfo";
 
-const photoUrl =
-  "https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80";
-
 const propertyInfo = [
   {
     price: 195000,
@@ -13,6 +10,8 @@ const propertyInfo = [
     sqft: 1568,
     "sqft-lot": 7000,
     address: "3051 Saint Daphne Dr, Saint Charles, MO 63301",
+    photoUrl:
+      "https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
   },
 ];
 
@@ -21,7 +20,7 @@ function App() {
     <div className="app">
       <h1>Real Estate</h1>
       <div className="info-container">
-        <Picture photoUrl={photoUrl}></Picture>
+        <Picture photoUrl={propertyInfo[0].photoUrl}></Picture>
         <PropertyInfo propertyInfo={propertyInfo}></PropertyInfo>
       </div>
     </div>
--- src/components/PropertyInfo.js
+++ src/components/PropertyInfo.js
@@ -6,6 +6,27 @@ function PropertyInfo({ propertyInfo }) {
       <h3 className="propertyPrice">
         ${propertyInfo[0].price.toLocaleString("en-US")}
       </h3>
+      <div>
+        <p className="propertyDetails">
+          <strong>{propertyInfo[0].bed}</strong> bed
+        </p>
+        <p className="propertyDetails">
+          <strong>{propertyInfo[0].bath}</strong> bath
+        </p>
+        <p className="propertyDetails">
+          <strong>{propertyInfo[0].sqft.toLocaleString("en-US")}</strong> sqft
+        </p>
+        <p className="propertyDetails">
+          <strong>{propertyInfo[0]["sqft-lot"].toLocaleString("en-US")}</strong>{" "}
+          sqft-lot
+        </p>
+      </div>
+      <div>
+        <p className="propertyAddress">{propertyInfo[0].address}</p>
+        <button type="button" className="changeProperty">
+          Next Property
+        </button>
+      </div>
     </div>
   );
 }
